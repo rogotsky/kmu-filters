4290d7830179869fcb8a9705c5d6041b34d1ed4e
rogotsky <p.rogotski@osahp.io>
2019-07-28 15:01:31 +0300

    minor fix

--- src/components/Filters/Stars.js
+++ src/components/Filters/Stars.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Stars = ({ count }) => {
   return (
     [...Array(count)].map((e, i) => <span className="dashicons dashicons-star-filled" key={i}/>)
   )
+};
+
+Stars.propTypes = {
+  count: PropTypes.number
 };
 
 export default Stars;
